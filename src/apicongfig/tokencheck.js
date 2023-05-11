a13c5a841f175f9638e1ff76415bbe93c99d35e7
digitalboy <digitalboyzone@gmail.com>
2023-05-11 15:43:39 +0800

    modified:   src/apicongfig/tokencheck.js

--- src/apicongfig/tokencheck.js
+++ src/apicongfig/tokencheck.js
@@ -1,6 +1,7 @@
 // src/axiosConfig.js
 import axios from "axios";
 import router from "@/router/index"; // 导入 router 实例
+import { ElMessage } from "element-plus";
 
 // 请求拦截器
 axios.interceptors.request.use(
@@ -26,7 +27,7 @@ axios.interceptors.response.use(
     if (error.response && error.response.status === 401) {
       // 清除本地存储的 token
       localStorage.removeItem("token");
-
+      ElMessage.error("请重新登录");
       // 使用 Vue Router 跳转到首页
       router.push("/");
     }
